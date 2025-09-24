8a516a3ef22f0f8970a6e3c2390164470f902561
JRT <joseryutoari@gmail.com>
2025-09-24 07:33:14 +0700

    Tugas 3 code

--- main/views.py
+++ main/views.py
@@ -1,10 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from main.forms import ProductForm
+from main.models import Product
+from django.http import HttpResponse
+from django.core import serializers
 
 def show_main(request):
+    products = Product.objects.all()
     context = {
-        'shop' : 'Football Shop',
+        "items": products,
+        'shop': 'Football Shop',
         'name': 'Manchaland Store',
-        'class': 'PBP E'
+        'class': 'PBP E',
     }
 
     return render(request, "main.html", context)
+
+def create_product(request):
+    form = ProductForm(request.POST or None)
+    if form.is_valid() and request.method == "POST":
+        form.save()
+        return redirect("main:show_main")
+    return render(request, "create_product.html", {"form": form})
+
+def show_product(request, id):
+    product = get_object_or_404(Product, pk=id)
+    return render(request, "product_detail.html", {"product": product})
+
+# XML, JSON, XML by ID, dan JSON by ID.
+def show_xml(request):
+    data = serializers.serialize("xml", Product.objects.all())
+    return HttpResponse(data, content_type="application/xml")
+
+def show_json(request):
+    data = serializers.serialize("json", Product.objects.all())
+    return HttpResponse(data, content_type="application/json")
+
+def show_xml_by_id(request, id):
+    data = serializers.serialize("xml", Product.objects.filter(pk=id))
+    return HttpResponse(data, content_type="application/xml")
+
+def show_json_by_id(request, id):
+    data = serializers.serialize("json", Product.objects.filter(pk=id))
+    return HttpResponse(data, content_type="application/json")
